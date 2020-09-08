a39a1e4f10df27f99e89658c194ded7a2cca07b9
Valentin P <valentin.porcellini@gmail.com>
2020-09-08 16:07:00 -0700

    Added form

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
+  form = new FormGroup({
+    email: new FormControl('')
+  });
 
-  constructor() { }
+  onSubmit() {
+    let data = this.form.value;
 
-  ngOnInit(): void {
+    return new Promise<any>((resolve, reject) => {
+      this.db
+        .collection('EarlyAccessEmails')
+        .doc(data.email)
+        .set(data)
+        .then(
+          (res) => {},
+          (err) => reject(err)
+        );
+    });
   }
 
+  constructor(private db: AngularFirestore) {}
+
+  ngOnInit(): void {}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBL5QRn9t-LKvJZJM-LxGWukckerv8OFNc",
+    authDomain: "virtual-menu-59b9e.firebaseapp.com",
+    databaseURL: "https://virtual-menu-59b9e.firebaseio.com",
+    projectId: "virtual-menu-59b9e",
+    storageBucket: "virtual-menu-59b9e.appspot.com",
+    messagingSenderId: "224436038053",
+    appId: "1:224436038053:web:31d1ef7108e07921b8604c",
+    measurementId: "G-DBGXXY409E"
+  }
 };
 
 /*
