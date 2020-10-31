d3a89ca5323e97941639d1bb8ddc416afbea9ce9
Valentin P <valentin.porcellini@gmail.com>
2020-10-30 22:31:47 -0700

    Updated screens

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,33 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
-import { AngularFirestore } from '@angular/fire/firestore';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {AngularFirestore} from '@angular/fire/firestore';
+declare var $ : any;
 
-@Component({
-  selector: 'app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css'],
-})
+@Component({selector: 'app-homepage', templateUrl: './homepage.component.html', styleUrls: ['./homepage.component.css']})
 export class HomepageComponent implements OnInit {
-  form = new FormGroup({
-    email: new FormControl('')
-  });
+    form = new FormGroup({email: new FormControl('')});
 
-  onSubmit() {
-    let data = this.form.value;
+    onSubmit() {
+        let data = this.form.value;
 
-    return new Promise<any>((resolve, reject) => {
-      this.db
-        .collection('EarlyAccessEmails')
-        .doc(data.email)
-        .set(data)
-        .then(
-          (res) => {},
-          (err) => reject(err)
-        );
-    });
-  }
+        return new Promise < any > ((resolve, reject) => {
+            this
+                .db
+                .collection('EarlyAccessEmails')
+                .doc(data.email)
+                .set(data)
+                .then((res) => {}, (err) => reject(err));
+        });
+    }
 
-  constructor(private db: AngularFirestore) {}
+    constructor(private db : AngularFirestore) {}
 
-  ngOnInit(): void {}
+    ngOnInit() : void {
+
+        $(document)
+            .on("click", function (evt) {
+
+                evt.preventDefault();
+
+                if ($(evt.target).parent().is('.tab')) {
+
+                    var screenSelected = $(evt.target)
+                        .parent()
+                        .data("anchor");
+
+                    $(".screens")
+                        .children()
+                        .each(function (index) {
+
+                            if ($(this).data("id") !== screenSelected) {
+                                $(this).removeClass("is-active");
+                            } else {
+                                $(this).addClass("is-active");
+                            }
+                        });
+
+                    $(evt.target)
+                        .parent()
+                        .parent()
+                        .children()
+                        .each(function (index) {
+                            $(this).removeClass("is-active");
+                        });
+
+                    $(evt.target)
+                        .parent()
+                        .addClass("is-active");
+                } else if ($(evt.target).is('.tab')) {
+                    var screenSelected = $(evt.target).data("anchor");
+
+                    $(".screens")
+                        .children()
+                        .each(function (index) {
+
+                            if ($(this).data("id") !== screenSelected) {
+                                $(this).removeClass("is-active");
+                            } else {
+                                $(this).addClass("is-active");
+                            }
+                        });
+
+                    $(evt.target)
+                        .parent()
+                        .children()
+                        .each(function (index) {
+                            $(this).removeClass("is-active");
+                        });
+
+                    $(evt.target).addClass("is-active");
+
+                }
+            });
+    }
+
 }
