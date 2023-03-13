2ca42e2763ef44788a47ad7a329a6c67f607b811
Rohan Tyagi <rohantyagi@Rohans-MacBook-Air.local>
2023-03-12 22:22:53 -0700

    Text, images, and videos

---
+++ src/app/homepage/script.js
@@ -0,0 +1,7 @@
+var iframe = document.createElement('iframe');
+iframe.src = '/assets/images/screens/order.png';
+iframe.width = '560';
+iframe.height = '315';
+
+var bottom = document.getElementById('step#order');
+bottom.appendChild(iframe);
