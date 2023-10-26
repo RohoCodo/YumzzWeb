30264f31a15a1f2c0f3f7c9f69fc387abb0801aa
Rohan Tyagi <rohantyagi@Rohans-MacBook-Air.local>
2023-10-26 16:01:08 -0700

    Updating AR experience

--- src/app/foodwaste-page/foodwaste.component.ts
+++ src/app/foodwaste-page/foodwaste.component.ts
@@ -10,22 +10,22 @@ declare var $ : any;
 })
 
 export class FoodWastePageComponent implements OnInit {
-  form = new FormGroup({email: new FormControl('')});
+//   form = new FormGroup({email: new FormControl('')});
 
-    onSubmit() {
-        let data = this.form.value;
-        this.form.reset();
-        alert("We will get back to you shortly!");
-        return new Promise < any > ((resolve, reject) => {
-            this
-                .db
-                .collection('EarlyAccessEmails')
-                .doc(data.email)
-                .set(data)
-                .then((res) => {}, (err) => reject(err));
-        });
+    // onSubmit() {
+    //     let data = this.form.value;
+    //     this.form.reset();
+    //     alert("We will get back to you shortly!");
+    //     return new Promise < any > ((resolve, reject) => {
+    //         this
+    //             .db
+    //             .collection('EarlyAccessEmails')
+    //             .doc(data.email)
+    //             .set(data)
+    //             .then((res) => {}, (err) => reject(err));
+    //     });
 
-    }
+    // }
 
     constructor(private db : AngularFirestore) {}
 
