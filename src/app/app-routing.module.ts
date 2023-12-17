cae1c8a1a6506502dbaf944c22b7302d844b9c34
Arya Tyagi <aryatyagi@Aryas-MacBook-Air.local>
2023-12-16 19:59:28 -0800

    added statistics page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { FoodWastePageComponent } from './foodwaste-page/foodwaste.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { StatisticsPageComponent } from './statistics-page/statistics-page.component';
 
 const routes: Routes = [
   { path: 'home', component: HomepageComponent},
   { path:'foodwaste', component: FoodWastePageComponent },
-  {path:'privacy', component: PrivacyPolicyComponent},
+  { path:'privacy', component: PrivacyPolicyComponent},
+  { path:'admin', component: StatisticsPageComponent},
   { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
 
